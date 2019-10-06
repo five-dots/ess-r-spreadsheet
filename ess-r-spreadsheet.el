05f6c691bae8ce3ecd4582e3ee731f7b7b533533
shun <syun.asai@gmail.com>
2019-10-06 16:10:53 +0900

    Comment out.

--- ess-r-spreadsheet.el
+++ ess-r-spreadsheet.el
@@ -37,19 +37,19 @@
 (require 'f)
 
 
-(defvar ess-r-spreadsheet-programs
-  '("libreoffice" "gnumeric" "openoffice" "soffice")
-  "List of spreadsheet programs in order of priority.")
+;; (defvar ess-r-spreadsheet-programs
+;;   '("libreoffice" "gnumeric" "openoffice" "soffice")
+;;   "List of spreadsheet programs in order of priority.")
 
-(defvar ess-r-spreadsheet--executable
-  (-first 'f-executable? (-map 'executable-find ess-r-spreadsheet-programs))
-  "Spreadsheet program to be used to view object.")
+;; (defvar ess-r-spreadsheet--executable
+;;   (-first 'f-executable? (-map 'executable-find ess-r-spreadsheet-programs))
+;;   "Spreadsheet program to be used to view object.")
 
-(defvar ess-r-spreadsheet-temp-dir "/tmp/ess-r-spreadsheet/"
-  "Temporary file directory.")
+;; (defvar ess-r-spreadsheet-temp-dir "/tmp/ess-r-spreadsheet/"
+;;   "Temporary file directory.")
 
-(defvar ess-r-spreadsheet--temp-file nil
-  "Temporary file.")
+;; (defvar ess-r-spreadsheet--temp-file nil
+;;   "Temporary file.")
 
 
 ;; (defun ess-r-spreadsheet--get-proc ()
